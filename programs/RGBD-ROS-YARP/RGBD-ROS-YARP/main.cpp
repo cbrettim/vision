16625a07ee104d3231089daac9fdf7a311fbfed2
davidvelascogarcia <davelasc@ing.uc3m.es>
2018-04-18 13:15:03 +0200

    Añadidas funciones bidireccionales ROS<->YARP

--- programs/RGBD-ROS-YARP/RGBD-ROS-YARP/main.cpp
+++ programs/RGBD-ROS-YARP/RGBD-ROS-YARP/main.cpp
@@ -1,5 +1,5 @@
-/*
- *
+/**************************************************************
+ **************************************************************
  *
  *      RGBD Sensor: TIAGo
  *
@@ -10,11 +10,15 @@
  *      Departamento de Ingeniería de Sistemas y Automática
  *      Universidad Carlos III de Madrid
  *
- *
- * */
+ **************************************************************
+ **************************************************************
+ */
 
-
+//***************************************
+//***************************************
 // Librerias
+//***************************************
+//***************************************
 
 #include <iostream>
 #include <yarp/os/all.h>
@@ -22,13 +26,21 @@
 #include "String.h"
 #include <image.h>
 
+//***************************************
+//***************************************
 // Espacios de nombres
+//***************************************
+//***************************************
 
 using namespace std;
 using namespace yarp::os;
 
 
+//***************************************
+//***************************************
 // Función main
+//***************************************
+//***************************************
 
 int main()
 {
@@ -43,20 +55,31 @@ int main()
 
     cout<<"Inicializando YARP"<<endl;
 
+    //***************************************
+    //***************************************
     // Inicialización YARP
+    //***************************************
+    //***************************************
 
     Network yarp;
 
+    //***************************************
     // Instanciar Bottles
+    //***************************************
 
     BufferedPort<Bottle> Botella_inPort, Botella_outPort;
 
+    //*****************************************************
     // Abrir puertos: Ahora no se emplean, para un futuro
+    //*****************************************************
 
     bool ok_in = Botella_inPort.open("/puerto_in");
     bool ok_out = Botella_outPort.open("/puerto_out");
 
+
+    //*********************************************
     // Comprobación puertos abiertos correctamente
+    //*********************************************
 
     if(ok_in==false){
 
@@ -69,11 +92,27 @@ int main()
     }
 
 
+    //****************************************
+    //****************************************
+    // Comunicación YARP - ROS: Bidireccional
+    //****************************************
+    //****************************************
+
+
+    //***************************************
+    //          YARP: Recibe de ROS
+    //***************************************
+
+
+    //***************************************
     // Instanciar nodo de escucha YARP
+    //***************************************
 
     Node node("/yarp/listener");
 
+    //***************************************
     // Suscribirse al topic de ROS
+    //***************************************
 
     yarp::os::Subscriber<String> subscriber;
 
@@ -85,20 +124,86 @@ int main()
         cout<<"La conexión con el topic de ROS se ha establecido correctamente"<<endl;
     }
 
+    //***************************************
     // Instanciamos objeto para guardarlo
+    //***************************************
 
     image Objeto_de_imagenes();
 
-    // Leemos datos del topic
 
-    while (true) {
+    //***************************************
+    //          YARP: Envía a ROS
+    //***************************************
 
-        String data;
-        subscriber.read(data);
-        cout << "Datos recibidos:" << data.data << " " << endl;
+
+
+    //***************************************
+    // Instanciamos nodo de YARP->ROS
+    //***************************************
+
+    Node node("/yarp/talker");
+
+    //********************************************
+    // Suscripción al topic de ROS que se prepare
+    //********************************************
+
+
+    yarp::os::Publisher<String> publisher;
+    if (!publisher.topic("/chatter")) {
+        cerr<< "Lo siento, YARP no se ha podido conectar con ROS en modo envio YARP->ROS";
+        return -1;
     }
+
+    String data;
+
+    //***************************************
+    // YARP: Lee de ROS
+    //***************************************
+
+    data=yarp_read_ros();
 
     return 0;
 }
 
-// Funciones parciales
+//***************************************
+// Funciones
+//***************************************
+
+//***************************************
+// YARP: Lee de ROS
+//***************************************
+
+String yarp_read_ros(){
+
+    //***************************************
+    // Leemos datos del topic
+    //***************************************
+
+        subscriber.read(data);
+        cout << "Datos recibidos:" << data.data << " " << endl;
+
+
+}
+
+//***************************************
+// YARP: Escribe a ROS
+//***************************************
+
+void yarp_write_ros(String data){
+
+    //***************************************
+    //          YARP: Envía a ROS
+    //***************************************
+
+        data.data="Esto es lo que envío";
+
+        //***************************************
+        // Publicamos a ROS
+        //***************************************
+
+        publisher.write(data);
+
+
+
+    }
+
