21230d4156a476c1e0b30e4bedbae8d0dc482711
davidvelascogarcia <davelasc@ing.uc3m.es>
2018-04-24 12:48:37 +0200

    comentarios clase image

--- programs/RGBD-ROS-YARP/RGBD-ROS-YARP/image.h
+++ programs/RGBD-ROS-YARP/RGBD-ROS-YARP/image.h
@@ -1,20 +1,66 @@
-#ifndef IMAGE_H
-#define IMAGE_H
+/**************************************************************
+ **************************************************************
+ *
+ *      RGBD Sensor: TIAGo
+ *
+ *      YARP Subscribe -> ROS Topic
+ *      Class: Image
+ *      Type: Headers
+ *
+ *      Robotics Lab
+ *      Departamento de Ingeniería de Sistemas y Automática
+ *      Universidad Carlos III de Madrid
+ *
+ **************************************************************
+ **************************************************************
+ */
 
-// Clase imagen
+#ifndef YARP_READ_H
+#define YARP_READ_H
 
+//***************************************
+//***************************************
+// Librerias
+//***************************************
+//***************************************
+
+#include <iostream>
+#include <yarp/os/all.h>
+#include <stdio.h>
+#include <image.h>
+
+
+#include <string>
+#include <vector>
+#include <sys/time.h>
+#include <unistd.h>
+#include <opencv2/opencv.hpp>
+
+// Include ROS mesage headers. Generated with yarpidl_rosmsgs
+
+#include "../msg/sensor_msgs_CameraInfo.h"
+#include "../msg/sensor_msgs_Image.h"
+#include "../msg/sensor_msgs_CompressedImage.h"
+#include "../msg/sensor_msgs_PointCloud2.h"
+
+#define TOPIC_IMAGE "/xtion/rgb/image_raw/compressed"
+
+//***************************************
+//***************************************
+// Espacios de nombres
+//***************************************
+//***************************************
+
+using namespace cv;
+using namespace std;
+using namespace yarp::os;
 class image
 {
 public:
-    // Constructor vacío
 
     image();
-
-    // Destructor
-
     ~image();
 
-    // Variables
 
 };
 
